5313cc46cc4cc1694860969ebbfc76b881648ebd
Tatiana <tshipilova392@yandex.ru>
2021-09-06 12:20:00 +0300

    ServerUp added

--- server/src/main/java/com/db/edu/team03/server/handler/Handler.java
+++ server/src/main/java/com/db/edu/team03/server/handler/Handler.java
@@ -2,5 +2,5 @@ package com.db.edu.team03.server.handler;
 
 public interface Handler {
 
-    public void accept(String message);
+    public void accept(String address, String message);
 }
--- server/src/main/java/com/db/edu/team03/server/handler/MessageHandler.java
+++ server/src/main/java/com/db/edu/team03/server/handler/MessageHandler.java
@@ -3,7 +3,7 @@ package com.db.edu.team03.server.handler;
 public class MessageHandler implements Handler {
 
     @Override
-    public void accept(String message) {
+    public void accept(String address, String message) {
 
     }
 }
