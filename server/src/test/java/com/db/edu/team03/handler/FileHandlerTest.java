6666d606e37d1193c442767c4ca13b03b3ac4700
hexecute <myaremchuk@yandex.ru>
2021-09-07 11:26:46 +0300

    disabled

--- server/src/test/java/com/db/edu/team03/handler/FileHandlerTest.java
+++ server/src/test/java/com/db/edu/team03/handler/FileHandlerTest.java
@@ -1,6 +1,7 @@
 package com.db.edu.team03.handler;
 
 import com.db.edu.team03.server.handler.FileHandler;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -13,6 +14,7 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+@Disabled
 public class FileHandlerTest {
     File file = mock(File.class);
     FileHandler fileHandler = new FileHandler(file);
