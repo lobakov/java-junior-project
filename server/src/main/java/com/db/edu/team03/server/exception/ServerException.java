cc7a5e85c01655772147c11317547addf6b8d99b
lobakov <lobakov@gmail.com>
2021-09-08 12:31:03 +0300

    fix: removed unused constructors

--- server/src/main/java/com/db/edu/team03/server/exception/ServerException.java
+++ server/src/main/java/com/db/edu/team03/server/exception/ServerException.java
@@ -1,24 +1,8 @@
 package com.db.edu.team03.server.exception;
 
 public class ServerException extends Exception {
-
-    public ServerException() {
-        super();
-    }
-
+    
     public ServerException(String message) {
         super(message);
-    }
-
-    public ServerException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ServerException(Throwable cause) {
-        super(cause);
-    }
-
-    protected ServerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
